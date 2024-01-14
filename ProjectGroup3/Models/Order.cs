d9518b719ca4008c65aef79c430bb273dc8071d5
Vanh020104 <114908216+Vanh020104@users.noreply.github.com>
2024-01-14 17:28:06 +0700

    code

---
+++ ProjectGroup3/Models/Order.cs
@@ -0,0 +1,14 @@
+﻿namespace ProjectGroup3.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public int UserId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal TotalAmount { get; set; }
+
+        // Navigation properties
+        public User User { get; set; }
+        public ICollection<OrderProduct> OrderProducts { get; set; }
+    }
+}
