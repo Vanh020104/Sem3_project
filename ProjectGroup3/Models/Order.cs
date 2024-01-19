bfcb9c69016f41d6f9e8253aadb7a104fd53ff80
Tien Dung <dungdtth2209011@fpt.edu.vn>
2024-01-19 16:43:09 +0700

    update Admin

--- ProjectGroup3/Models/Order.cs
+++ ProjectGroup3/Models/Order.cs
@@ -6,7 +6,8 @@
         public int UserId { get; set; }
         public DateTime OrderDate { get; set; }
         public decimal TotalAmount { get; set; }
-
+        public bool Status { get; set; }  // Trạng thái đơn hàng (đã xác nhận hay chưa)
+        public bool IsPaid { get; set; }
         // Navigation properties
         public User User { get; set; }
         public ICollection<OrderProduct> OrderProducts { get; set; }
